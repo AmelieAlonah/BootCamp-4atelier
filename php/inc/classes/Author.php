719ee85ffdb172f65307f72d46e854e95fd01101
AmelieAlonah <amelie.abdallah@hotmail.fr>
2021-07-11 16:36:13 +0200

    classe author

---
+++ php/inc/classes/Author.php
@@ -0,0 +1,21 @@
+<?php
+
+class Author {
+  private $name;
+
+  public function __construct($nameParam)
+  {
+    $this->name = $nameParam;
+  }
+
+  public function getName()
+  {
+    return $this->name;
+  }
+  
+  public function setName($newName){
+    
+    $this->name = $newName;
+  }
+
+}
